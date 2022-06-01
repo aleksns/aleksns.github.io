174dae75ff8713b3a6ff5e47f317392629fc9f52
Aleksns <aleksnmsg@gmail.com>
2022-06-01 13:39:15 +0300

    changed profile photo

--- src/sections/HeroSection.js
+++ src/sections/HeroSection.js
@@ -4,7 +4,7 @@ export default function HeroSection() {
   return (
     <>
       <div className="hero-image hero-image-profile">
-        <div className="hero-text-container">
+        <div className="hero-text-container hero-text-container-profile">
           <h1>ALEKSANDR NAZAROV</h1>
           <h3>Software Developer</h3>
         </div>
